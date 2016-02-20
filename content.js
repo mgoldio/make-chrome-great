1628fe4ac2c7ee536219092c6dfe10bf5c32a4ea
Noah Prince <noahprince8@gmail.com>
2016-02-20 14:19:28 -0600

    Improve consistency. Attempt added elements

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-window.plot = function(img, x, y, w, h) {
+window.overlayTrump = function(img, x, y, w, h) {
   var trump=document.createElement('img');
   trump.src =chrome.extension.getURL('trump_cutouts/trump_cutout01.png');
   trump.style.width = 1.1*w + "px";
@@ -9,6 +9,8 @@ window.plot = function(img, x, y, w, h) {
 
   trump.onload = function () {
     var wrapper = document.createElement("div");
+    wrapper.class = "trumpWrapper";
+    wrapper.style.position = "relative";
     img.parentElement.appendChild(wrapper);
     img.parentElement.removeChild(img);
 
@@ -17,15 +19,33 @@ window.plot = function(img, x, y, w, h) {
   }
 };
 
-$.each($("img"),function(index, img){
+window.trumpify = function(img) {
   var tracker = new tracking.ObjectTracker(['face']);
   tracker.setStepSize(1.7);
 
   tracking.track(img, tracker);
   tracker.on('track', function(event) {
     event.data.forEach(function(rect) {
-      window.plot(img, rect.x, rect.y, rect.width, rect.height);
+      window.overlayTrump(img, rect.x, rect.y, rect.width, rect.height);
     });
-  });    
+  });
+};
+
+$(document).ready(function(){
+  $.each($("img"),function(index, img){
+    window.trumpify(img);
+  });
+
+  $(document).bind('DOMNodeInserted',function(e){
+    if ($(e.target).is("img")){
+      // Not already trump
+      if($(e.target).parent(".trumpWrapper").size == 0) {
+        debugger;
+        window.trumpify(e.target);
+      }
+    }
+  });
 });
+
+
 //chrome.runtime.sendMessage({"message": "send_photo", "photos": photos});
