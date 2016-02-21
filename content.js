6a5be65f3232029aaaa910273c78dffe66245d59
Michael Goldstein <mjgolds2@illinois.edu>
2016-02-21 00:23:41 -0600

    text replacement working ;)

--- content.js
+++ content.js
@@ -30,11 +30,30 @@ window.trumpify = function(img) {
   tracker.on("track", function(event) {
     event.data.forEach(function(rect) {
       window.overlayTrump(img, rect.x, rect.y, rect.width, rect.height);
-    });
+});
   });
 };
 
 $(document).ready(function(){
+  $.getJSON(chrome.extension.getURL('replacements.json'), function(r) {
+    var textElems = ["a", "title", "p", "span", "h1", "h2", "h3", "h4", "h5", "h6", "li"];
+    for(var j = 0; j < textElems.length; j++) {
+      var type = textElems[j];
+      $(type).each(function(index, elem) {
+        var text = $(elem).html();
+        var djt = text;
+        for(var i = 0; i < r.length; i++) {
+          var repl = r[i];
+          djt = djt.replace(new RegExp(repl.word, "g"), 
+            repl.replacements[Math.floor(Math.random() 
+            * repl.replacements.length)]);
+        }
+        if(djt != text)
+          $(elem).html(djt);
+      });
+    }
+  });
+
   $.each($("img"),function(index, img){
     if(($(img).attr("isTrumpified") != "true") && isOnScreen(img)) {
       $(img).attr("isTrumpified", "true");
